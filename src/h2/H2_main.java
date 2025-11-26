b29708bee642c36fcd450aee89a0eb386e5a54e6
brickdelivery <jburgma2@smail.uni-koeln.de>
2025-11-26 22:41:12 +0100

    restructure h2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -8,12 +8,11 @@ public class H2_main {
     public static boolean compareArrays(int[] arr1, int[] arr2) {
         if (arr1.length == arr2.length) {
             for (int i = 0; i < arr1.length; i++) {
-                if (arr1[i] != arr2[i]) {
+                if (arr1[i] != arr2[i])
                     return false;
-                }
             }
-            return true;
-        }
-        return false;
+        } else
+            return false;
+        return true;
     }
 }
