92bb6520dc40c8db4dca78c411666106a0069a07
brickdelivery <jburgma2@smail.uni-koeln.de>
2025-11-26 22:22:18 +0100

    rename parameter variables

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -5,12 +5,11 @@ public class H1_main {
         //System.out.println(isMirrorArray(new int[]{4, 3, 2}, new int[]{2, 3, 4}));
     }
 
-    public static boolean isMirrorArray(int[] a, int[] b) {
-        if (a.length == b.length) {
-            for (int i = 0; i < a.length; i++) {
-                if (a[i] != b[b.length - 1 - i])
+    public static boolean isMirrorArray(int[] arr1, int[] arr2) {
+        if (arr1.length == arr2.length) {
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i] != arr2[arr2.length - 1 - i])
                     return false;
-
             }
         } else
             return false;
