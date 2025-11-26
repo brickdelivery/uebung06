c0f7455e7cbf5eb86f896a50403e694ce17313af
brickdelivery <jburgma2@smail.uni-koeln.de>
2025-11-26 20:12:55 +0100

    add p1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class P1_main {
+    public static void main(String[] args) {
+        //System.out.print(squared(8));
+    }
+
+    public static int squared(int n) {
+        return (n+1)*(n+1);
+    }
+
+
+}
+
